16d6be09eadfaa0f9fc204b6e7ebc413f45ae6a9
nicolas wang <nicolas.xian@rappi.com>
2020-09-08 12:36:23 -0300

    request created and setted entity created and setted

---
+++ RedditPost/RedditPost/Core/Entity/RedditModel.swift
@@ -0,0 +1,27 @@
+//
+//  RedditModel.swift
+//  RedditPost
+//
+//  Created by Nicolas Wang on 08/09/2020.
+//  Copyright © 2020 Nicolas Wang. All rights reserved.
+//
+
+import Foundation
+
+struct RedditModel: Codable {
+    var kind: String?
+    var data: RedditPostModel?
+}
+
+struct RedditPostModel: Codable {
+    var id: String?
+    var title: String?
+    var author: String?
+    var numComments: Int?
+    var thumbnail: String?
+    var createdUtc: Double?
+    var readed: Bool = false
+    var visited: Bool = false
+    var saved: Bool = false
+    var authorFullname: String?
+}
