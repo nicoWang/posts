cec95f090cad82460cf236224ed5424bc43c1e2e
nicolas wang <nicolas.xian@rappi.com>
2020-09-08 11:32:31 -0300

    Project created

---
+++ RedditPost/RedditPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditPost
+//
+//  Created by Nicolas Wang on 08/09/2020.
+//  Copyright © 2020 Nicolas Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
